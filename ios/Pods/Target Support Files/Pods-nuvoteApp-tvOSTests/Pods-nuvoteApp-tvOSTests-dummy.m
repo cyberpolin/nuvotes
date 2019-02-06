1e514b0731b0d546dfe8e2148cd03c69e52c3bd0
luismendoza <luismendoza947@hotmail.com>
2019-02-06 11:07:49 -0600

    added pods and ios build

---
+++ ios/Pods/Target Support Files/Pods-nuvoteApp-tvOSTests/Pods-nuvoteApp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nuvoteApp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_nuvoteApp_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-nuvoteAppTests/Pods-nuvoteAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nuvoteAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_nuvoteAppTests
+@end
