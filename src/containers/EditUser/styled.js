d145ac8c533b447a0560592301e609ec86fa9aa4
luismendoza <luismendoza947@hotmail.com>
2019-02-12 10:48:35 -0600

    created json with prop styles and added styles for big screen or ios

--- src/containers/EditUser/styled.js
+++ src/containers/EditUser/styled.js
@@ -1,12 +1,40 @@
-import { Platform } from 'react-native'
+import { Platform, Dimensions } from 'react-native'
 import styled from 'styled-components/native'
+import { black, white } from '../../colorPalette'
+
+export const styles = {
+  inputStyle: {
+    fontSize: 14,
+    minHeight: 30,
+    padding: 0
+  },
+  containerStyle: {
+    paddingHorizontal: 0
+  },
+  buttonStyle: {
+    backgroundColor: white,
+    borderWidth: 1,
+    borderColor: black
+  },
+  titleStyle: {
+    color: black
+  },
+  buttonContainerStyle: {
+    width: '40%',
+    alignSelf: 'center',
+    marginTop: 'auto'
+  },
+  iconContainerStyle: {
+    height: 150
+  },
+  keyboardScrollViewContentStyle: {
+    flex: 1,
+    alignItems: 'center'
+  }
+}
 
 const isIOS = Platform.OS === 'ios'
-
-export const Container = styled.View`
-  flex: 1;
-  align-items: center;
-`
+const isBigScreen = Dimensions.get('window').height > 680
 
 export const Text = styled.Text`
   margin-bottom: 5%;
@@ -23,15 +51,16 @@ export const InfoContainer = styled.View`
 `
 
 export const CircleImage = styled.Image`
-  width: 150px;
-  height: 150px;
-  border-radius: 75px;
+  width: 120px;
+  height: 120px;
+  border-radius: ${isIOS ? '60px' : '80px'};
   ${isIOS && 'border-color: #000'};
   ${isIOS && 'border-width: 1px'};
 `
 
 export const ImageContainer = styled.View`
   padding-top: 10%;
+  ${isBigScreen && 'margin-bottom: 20px'};
 `
 
 export const FlexRow = styled.View`
@@ -41,5 +70,5 @@ export const FlexRow = styled.View`
 
 export const InputContainer = styled.View`
   width: ${({width}) => width ? `${width}%` : '60%'};
-  margin-bottom: 10%;
+  margin-bottom: 5%;
 `
