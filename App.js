5320cfdd97da9ca440857cec90d96af9df0a66c3
luismendoza <luismendoza947@hotmail.com>
2019-02-13 15:34:18 -0600

    changed routes for view user container for testing

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import Routes from './src/routes'
+import { ViewUser } from './src/containers'
 
 export default class App extends Component {
   render () {
-    return <Routes />
+    return <ViewUser />
   }
 }
