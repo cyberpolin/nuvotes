1e03f45cd70dc2c964c0fe5cafce19714ea09cb4
luismendoza <luismendoza947@hotmail.com>
2019-02-13 15:39:44 -0600

    added onPress navigate to sign in button

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -14,6 +14,7 @@ import {
 
 export default class Login extends Component {
   render () {
+    const { navigation } = this.props
     return (
       <Container>
         <AvoidingView
@@ -45,6 +46,7 @@ export default class Login extends Component {
               titleStyle={styles.buttonTitle}
               containerStyle={styles.buttonContainer}
               buttonStyle={styles.buttonStyle}
+              onPress={() => navigation.navigate('Home')}
             />
           </LoginBox>
         </AvoidingView>
