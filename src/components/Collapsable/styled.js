8c4000b3905bcd6b94986a3138e878fbd17f7bf0
luismendoza <luismendoza947@hotmail.com>
2019-02-13 11:17:38 -0600

    added padding to element

--- src/components/Collapsable/styled.js
+++ src/components/Collapsable/styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/native'
 export const Button = styled.TouchableOpacity`
   flex-direction: row;
   align-self: flex-start;
+  padding: 5px 10px 5px 0;
 `
 
 export const Label = styled.Text`
