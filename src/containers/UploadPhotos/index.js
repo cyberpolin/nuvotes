94e0cf31cc9e420bcfff3dd801bd5d5217ab2c2a
luismendoza <luismendoza947@hotmail.com>
2019-02-13 11:58:23 -0600

    created uploadPhotos container

---
+++ src/containers/UploadPhotos/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native'
+
+export default class UploadPhotos extends Component {
+  render () {
+    return (
+      <View>
+        <Text>UPLOAD PHOTOS</Text>
+      </View>
+    )
+  }
+}
