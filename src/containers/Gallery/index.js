f81ad10464cb37a25aa6fc3294cd63639586cf76
luismendoza <luismendoza947@hotmail.com>
2019-02-12 18:13:31 -0600

    created gallery container

---
+++ src/containers/Gallery/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class Gallery extends Component {
+  render () {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>GALLERY SCREEN</Text>
+      </View>
+    )
+  }
+}
