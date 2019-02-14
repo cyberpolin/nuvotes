5dcc675fa23d83299dc67d7b274368472ce66dc0
luismendoza <luismendoza947@hotmail.com>
2019-02-14 13:09:00 -0600

    added scrollview for photos and methods to get photos from camera and gallery and delete them

--- src/containers/UploadPhotos/index.js
+++ src/containers/UploadPhotos/index.js
@@ -1,42 +1,128 @@
 import React, { Component } from 'react'
+import {
+  ScrollView,
+  ActivityIndicator,
+  View,
+  Platform
+} from 'react-native'
 import ImagePicker from 'react-native-image-crop-picker'
 import {
   Text,
   Icon,
-  Button
+  Button,
+  Image
 } from 'react-native-elements'
 import {
   Container,
-  FlexRow
+  FlexRow,
+  ButtonsContainer,
+  ImagesContainer,
+  styles
 } from './styled'
 
 export default class UploadPhotos extends Component {
   constructor (props) {
     super(props)
-    this.selectFromGallery = this.selectFromGallery.bind(this)
+    this.state = {
+      photos: []
+    }
   }
   render () {
+    const { photos } = this.state
     return (
       <Container>
         <Text h4>Photos</Text>
+        {photos.length > 0 &&
+          <ScrollView
+            bounces={false}
+            contentContainerStyle={styles.scrollViewContainer}
+          >
+            <ImagesContainer>
+              {this.renderImages()}
+            </ImagesContainer>
+          </ScrollView>
+        }
+        <ButtonsContainer>
+          <Button
+            buttonStyle={{...styles.buttonStyle, ...styles.marginButton}}
+            titleStyle={styles.buttonTitle}
+            type='outline'
+            title='Take a photo...'
+            onPress={() => this.selectFromCamera()}
+          />
+          <Button
+            buttonStyle={styles.buttonStyle}
+            titleStyle={styles.buttonTitle}
+            type='outline'
+            title='Choose from the gallery...'
+            onPress={() => this.selectFromGallery()}
+          />
+        </ButtonsContainer>
         <FlexRow>
           <Button
+            buttonStyle={styles.buttonStyle}
+            titleStyle={styles.buttonTitle}
+            type='outline'
             title='Cancel'
           />
           <Button
+            buttonStyle={styles.buttonStyle}
+            titleStyle={styles.buttonTitle}
+            type='outline'
             title='Save'
-            onPress={() => this.selectFromGallery()}
           />
         </FlexRow>
       </Container>
     )
   }
 
+  renderImages () {
+    const { photos } = this.state
+    const isAndroid = Platform.OS === 'android'
+    return photos.map((photo, index) => {
+      const { sourceURL, path } = photo
+      return (
+        <View key={index}>
+          <Image
+            source={{uri: isAndroid ? path : sourceURL}}
+            style={styles.image}
+            PlaceholderContent={<ActivityIndicator />}
+          />
+          <Icon
+            type='font-awesome'
+            name='times'
+            containerStyle={styles.deleteIcon}
+            color='red'
+            onPress={() => this.deletePhoto(index)}
+            underlayColor='transparent'
+          />
+        </View>
+      )
+    })
+  }
+
   selectFromGallery () {
     ImagePicker.openPicker({
       multiple: true
-    }).then(image => {
-      console.log(image)
+    }).then(images => {
+      const { photos } = this.state
+      const newPhotosArray = [...photos, ...images]
+      this.setState({photos: newPhotosArray})
     })
+  }
+
+  selectFromCamera () {
+    ImagePicker.openCamera({
+    }).then(image => {
+      const { photos } = this.state
+      const newPhotosArray = [...photos, image]
+      this.setState({photos: newPhotosArray})
+    })
+  }
+
+  deletePhoto (index) {
+    let { photos } = this.state
+    photos.splice(index, 1)
+    this.setState({photos})
   }
 }
