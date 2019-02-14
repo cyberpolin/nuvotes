800be9aacdc9aaddd3992a19ab054b476df64803
luismendoza <luismendoza947@hotmail.com>
2019-02-14 16:29:29 -0600

    Added buttons and title to container

--- src/containers/UploadFiles/index.js
+++ src/containers/UploadFiles/index.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react'
-import {View, Text} from 'react-native'
+import {
+  Button,
+  Text
+} from 'react-native-elements'
+import {
+  Container,
+  FlexRow,
+  styles
+} from './styled'
 
 export default class UploadFiles extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isVisible: props.isVisible
+    }
+  }
   render () {
     return (
-      <View>
-        <Text>UPLOAD FILES</Text>
-      </View>
+      <Container>
+        <Text h4>Files</Text>
+        <Button
+          buttonStyle={styles.buttonStyle}
+          titleStyle={styles.buttonTitle}
+          type='outline'
+          title='Select a File...'
+        />
+        <FlexRow>
+          <Button
+            buttonStyle={styles.buttonStyle}
+            titleStyle={styles.buttonTitle}
+            type='outline'
+            title='Cancel'
+            onPress={() => this.handleClose()}
+          />
+          <Button
+            buttonStyle={styles.buttonStyle}
+            titleStyle={styles.buttonTitle}
+            type='outline'
+            title='Save'
+          />
+        </FlexRow>
+      </Container>
     )
+  }
+
+  handleClose () {
+    const { changeVisibility } = this.props
+    const { isVisible } = this.state
+    changeVisibility(!isVisible)
   }
 }
