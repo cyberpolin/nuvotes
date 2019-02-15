c8003e462c08b1795f84df727239c56a12cbdc5b
luismendoza <luismendoza947@hotmail.com>
2019-02-14 18:26:04 -0600

    added selectFile method to get files and render Files icons

--- src/containers/UploadFiles/index.js
+++ src/containers/UploadFiles/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-import { DocumentPicker } from 'react-native-document-picker'
+import {
+  View,
+  ScrollView
+} from 'react-native'
+import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker'
 import {
   Button,
-  Text
+  Text,
+  Icon
 } from 'react-native-elements'
 import {
   Container,
   FlexRow,
+  IconsContainer,
   styles
 } from './styled'
 
@@ -14,13 +20,15 @@ export default class UploadFiles extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      isVisible: props.isVisible
+      isVisible: props.isVisible,
+      files: []
     }
   }
   render () {
     return (
       <Container>
         <Text h4>Files</Text>
+        {this.renderFiles()}
         <Button
           buttonStyle={styles.buttonStyle}
           titleStyle={styles.buttonTitle}
@@ -47,11 +55,38 @@ export default class UploadFiles extends Component {
     )
   }
 
+  renderFiles () {
+    const { files } = this.state
+    if (files.length > 0) {
+      return (
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+          <IconsContainer>
+            {files.map((file, index) => {
+              return (
+                <View key={index}>
+                  <Icon
+                    type='font-awesome'
+                    name='file'
+                  />
+                </View>
+              )
+            })}
+          </IconsContainer>
+        </ScrollView>
+      )
+    }
+  }
+
   selectFile () {
     DocumentPicker.show({
+      filetype: [DocumentPickerUtil.pdf()]
     }, (error, res) => {
       console.log(error)
-      console.log(res)
+      if (res) {
+        const { files } = this.state
+        const newFilesArray = [...files, res]
+        this.setState({files: newFilesArray})
+      }
     })
   }
 
