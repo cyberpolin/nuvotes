f4dcd08d10429cc34db0111b365adbfb77a62b04
luismendoza <luismendoza947@hotmail.com>
2019-02-15 17:53:01 -0600

    created list to render files

--- src/containers/DocumentsScreen/index.js
+++ src/containers/DocumentsScreen/index.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { Card, List } from 'react-native-elements'
 import {
-  Container
+  ListItem,
+  Text,
+  Icon
+} from 'react-native-elements'
+import {
+  Container,
+  ScrollContainer,
+  FlexRow,
+  styles
 } from './styled'
 
 export default class DocumentsScreen extends Component {
   render () {
+    const files = []
     return (
       <Container>
-        <Text>DOCUMENTS</Text>
+        {files.length > 0
+          ? <ScrollContainer>{this.renderFiles(files)}</ScrollContainer>
+          : <Text>There are no files to show.</Text>}
       </Container>
+    )
+  }
+
+  renderFiles (files) {
+    return files.map((file, index) => {
+      const { fileName } = file
+      return (
+        <ListItem
+          key={index}
+          leftIcon={{name: 'file', type: 'font-awesome'}}
+          rightElement={this.renderRightItem()}
+          title={fileName}
+          bottomDivider
+          rightSubtitle='File size: 200kb'
+        />
+      )
+    })
+  }
+
+  renderRightItem () {
+    return (
+      <FlexRow>
+        <Icon
+          name='download'
+          type='font-awesome'
+          containerStyle={styles.marginRight}
+        />
+        <Icon
+          name='times'
+          type='font-awesome'
+        />
+      </FlexRow>
     )
   }
 }
