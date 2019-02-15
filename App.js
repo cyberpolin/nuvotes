0e6871960355e5c6dba4793f08b516e1caff0cf9
luismendoza <luismendoza947@hotmail.com>
2019-02-15 12:46:29 -0600

    changed routes from editUser container

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import Routes from './src/routes'
-import { EditUser } from './src/containers'
 
 export default class App extends Component {
   render () {
-    return <EditUser />
+    return <Routes />
   }
 }
