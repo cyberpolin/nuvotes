97dfd543d406b93122587cb4f9c550a48a5452bc
luismendoza <luismendoza947@hotmail.com>
2019-02-15 16:54:56 -0600

    created documents screen container

---
+++ src/containers/DocumentsScreen/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Card, List } from 'react-native-elements'
+import {
+  Container
+} from './styled'
+
+export default class DocumentsScreen extends Component {
+  render () {
+    return (
+      <Container>
+        <Text>DOCUMENTS</Text>
+      </Container>
+    )
+  }
+}
---
+++ src/containers/DocumentsScreen/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.ScrollView.attrs({
+  bounces: false,
+  contentContainerStyle: {
+    padding: 15
+  }
+})`
+  flex: 1;
+`
