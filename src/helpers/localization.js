a76ac39386f68b6700f0c3047f1514984272fd67
luismendoza <luismendoza947@hotmail.com>
2019-02-18 16:21:25 -0600

    created translation languages

---
+++ src/helpers/localization.js
@@ -0,0 +1,26 @@
+import LocalizedStrings from 'react-native-localization'
+
+export const translate = new LocalizedStrings({
+  en: {
+    signIn: 'Sign In',
+    password: 'Password',
+    userName: 'Username',
+    changeLanguage: 'Change Language',
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    state: 'State',
+    address: 'Address',
+    email: 'Email'
+  },
+  es: {
+    signIn: 'Iniciar Sesión',
+    password: 'Contraseña',
+    userName: 'Usuario',
+    changeLanguage: 'Cambiar Idioma',
+    firstName: 'Nombre',
+    lastName: 'Apellido',
+    state: 'Estado',
+    address: 'Dirección',
+    email: 'Correo Electrónico'
+  }
+})
