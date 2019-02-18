06686aa3268dd1f66946a5310f28c2d9eaac4d96
luismendoza <luismendoza947@hotmail.com>
2019-02-18 10:31:59 -0600

    changed ViewUser container to Routes

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import Routes from './src/routes'
-import { ViewUser } from './src/containers'
 
 export default class App extends Component {
   render () {
-    return <ViewUser />
+    return <Routes />
   }
 }
