5849cf4a0f8a2692416a0c25d28870aaddf9ea03
luismendoza <luismendoza947@hotmail.com>
2019-02-18 17:13:34 -0600

    implemented redux store to App.js

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
 import Routes from './src/routes'
+import store from './src/store'
 
 export default class App extends Component {
   render () {
-    return <Routes />
+    return (
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    )
   }
 }
