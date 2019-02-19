7e3fcf79f023b7d709c567392b04181302b5921d
luismendoza <luismendoza947@hotmail.com>
2019-02-19 17:51:26 -0600

    refactored code to put translations on separated files

--- src/helpers/localization.js
+++ src/helpers/localization.js
@@ -1,51 +1,11 @@
 import LocalizedStrings from 'react-native-localization'
 import STORE from '../store'
+import en from './english'
+import es from './spanish'
 
 export const translate = new LocalizedStrings({
-  en: {
-    signIn: 'Sign In',
-    password: 'Password',
-    userName: 'Username',
-    changeLanguage: 'Change Language',
-    firstName: 'First Name',
-    lastName: 'Last Name',
-    state: 'State',
-    address: 'Address',
-    email: 'Email',
-    signOut: 'Sign Out',
-    profile: 'Profile',
-    languageAlertTitle: 'Change Language',
-    languageAlertDescription: 'Do you want to change the language to spanish?',
-    yes: 'Yes',
-    orders: 'Orders',
-    inProgress: 'In Progress',
-    overdue: 'Overdue',
-    inspection: 'Inspection',
-    insurance: 'Insurance',
-    repair: 'Repair'
-  },
-  es: {
-    signIn: 'Iniciar Sesión',
-    password: 'Contraseña',
-    userName: 'Usuario',
-    changeLanguage: 'Cambiar Idioma',
-    firstName: 'Nombre',
-    lastName: 'Apellido',
-    state: 'Estado',
-    address: 'Dirección',
-    email: 'Correo Electrónico',
-    signOut: 'Cerrar Sesión',
-    profile: 'Perfil',
-    languageAlertTitle: 'Cambiar Idioma',
-    languageAlertDescription: '¿Desea cambiar el idioma a inglés?',
-    yes: 'Sí',
-    orders: 'Órdenes',
-    inProgress: 'En Proceso',
-    overdue: 'Atrasados',
-    inspection: 'Inspección',
-    insurance: 'Seguro',
-    repair: 'Reparación'
-  }
+  en,
+  es
 })
 
 export const checkLanguage = () => {
