3899336fd52c80333200cf83aa51613199308b87
luismendoza <luismendoza947@hotmail.com>
2019-02-19 17:17:25 -0600

    put checkLanguage function onBeforeLift prop from PersistGate

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import Routes from './src/routes'
 import STORE from './src/store'
+import { checkLanguage } from './src/helpers/localization'
 const { store, persistor } = STORE
 
 export default class App extends Component {
   render () {
     return (
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        <PersistGate
+          loading={null}
+          persistor={persistor}
+          onBeforeLift={checkLanguage}
+        >
           <Routes />
         </PersistGate>
       </Provider>
