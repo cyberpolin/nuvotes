b0e8b7347db742d2a43419dd1bb32c697ebf2f72
luismendoza <luismendoza947@hotmail.com>
2019-02-19 10:16:56 -0600

    created styled component for scrollview and textContainer

--- src/containers/Orders/styled.js
+++ src/containers/Orders/styled.js
@@ -1,7 +1,17 @@
-export const styles = {
-  containerStyle: {
+import styled from 'styled-components/native'
+
+export const Container = styled.ScrollView.attrs({
+  bounces: false,
+  contentContainerStyle: {
     alignItems: 'center',
     paddingVertical: 15,
     flex: 1
   }
-}
+})`
+`
+
+export const TextContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
