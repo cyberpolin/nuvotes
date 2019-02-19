054368aae15889e3c08e42c94613f98c5783fd1c
luismendoza <luismendoza947@hotmail.com>
2019-02-18 18:20:53 -0600

    implemented redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import reducers from '../reducers'
 
@@ -14,7 +15,7 @@ const persistedReducer = persistReducer(persistConfig, reducers)
 export default (() => {
   const store = createStore(
     persistedReducer,
-    compose(applyMiddleware(logger)))
+    compose(applyMiddleware(thunk, logger)))
   const persistor = persistStore(store)
   return { persistor, store }
 })()
