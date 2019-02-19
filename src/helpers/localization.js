8c2b024c99539a2b0f1df26c8a06a8b34cb200df
luismendoza <luismendoza947@hotmail.com>
2019-02-19 17:18:50 -0600

    created function to checkLanguage on start and change language. new keywords for translate

--- src/helpers/localization.js
+++ src/helpers/localization.js
@@ -1,4 +1,5 @@
 import LocalizedStrings from 'react-native-localization'
+import STORE from '../store'
 
 export const translate = new LocalizedStrings({
   en: {
@@ -10,7 +11,18 @@ export const translate = new LocalizedStrings({
     lastName: 'Last Name',
     state: 'State',
     address: 'Address',
-    email: 'Email'
+    email: 'Email',
+    signOut: 'Sign Out',
+    profile: 'Profile',
+    languageAlertTitle: 'Change Language',
+    languageAlertDescription: 'Do you want to change the language to spanish?',
+    yes: 'Yes',
+    orders: 'Orders',
+    inProgress: 'In Progress',
+    overdue: 'Overdue',
+    inspection: 'Inspection',
+    insurance: 'Insurance',
+    repair: 'Repair'
   },
   es: {
     signIn: 'Iniciar Sesión',
@@ -21,11 +33,32 @@ export const translate = new LocalizedStrings({
     lastName: 'Apellido',
     state: 'Estado',
     address: 'Dirección',
-    email: 'Correo Electrónico'
+    email: 'Correo Electrónico',
+    signOut: 'Cerrar Sesión',
+    profile: 'Perfil',
+    languageAlertTitle: 'Cambiar Idioma',
+    languageAlertDescription: '¿Desea cambiar el idioma a inglés?',
+    yes: 'Sí',
+    orders: 'Órdenes',
+    inProgress: 'En Proceso',
+    overdue: 'Atrasados',
+    inspection: 'Inspección',
+    insurance: 'Seguro',
+    repair: 'Reparación'
   }
 })
 
-export const getLanguageToChange = (language) => {
-  const languageToChange = language === 'es' ? 'en' : 'es'
-  return translate.setLanguage(languageToChange)
+export const checkLanguage = () => {
+  const { selectedLanguage } = STORE.store.getState().language
+  const currentLanguage = translate.getLanguage()
+  if (currentLanguage !== selectedLanguage) {
+    translate.setLanguage(selectedLanguage)
+  }
+}
+
+export const setLanguage = () => {
+  const currentLanguage = translate.getLanguage()
+  const languageToChange = currentLanguage === 'en' ? 'es' : 'en'
+  translate.setLanguage(languageToChange)
+  return languageToChange
 }
