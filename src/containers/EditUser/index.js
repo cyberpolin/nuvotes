faa1665253665a92ebc70b349fb1205bc9f94648
luismendoza <luismendoza947@hotmail.com>
2019-02-20 11:52:32 -0600

    replaced text for translation text

--- src/containers/EditUser/index.js
+++ src/containers/EditUser/index.js
@@ -161,7 +161,7 @@ export default class EditUser extends Component {
 
   showImagePicker () {
     const options = {
-      title: 'Select Avatar',
+      title: translate.selectAvatar,
       takePhotoButtonTitle: `${translate.takePhoto}...`,
       chooseFromLibraryButtonTitle: `${translate.chooseGallery}...`,
       cancelButtonTitle: translate.cancel
