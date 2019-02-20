7eacab04ba276384afbb2d77750848aa9ae084c6
luismendoza <luismendoza947@hotmail.com>
2019-02-20 11:38:27 -0600

    deleted unused code

--- src/containers/Orders/index.js
+++ src/containers/Orders/index.js
@@ -1,46 +1,15 @@
 import React, { Component } from 'react'
-import { Button } from 'react-native'
-import { connect } from 'react-redux'
-import { changeLanguage } from '../../actions/language'
-import { getLanguageToChange } from '../../helpers/localization'
 import {
   Container,
   Text
 } from './styled'
 
-class Orders extends Component {
+export default class Orders extends Component {
   render () {
-    const { changeLanguage } = this.props
     return (
       <Container>
         <Text>ORDERS SCREEN</Text>
-        <Button
-          title='TO PROFILE'
-          onPress={() => this.props.navigation.navigate('Profile')}
-        />
-        <Button
-          title='Change Language'
-          onPress={() => this.handleLanguage()}
-        />
       </Container>
     )
   }
-
-  handleLanguage () {
-    console.log(getLanguageToChange())
-  }
 }
-
-const mapStateToProps = ({language}) => ({
-  language
-})
-
-const mapDispatchToProps = dispatch => {
-  return {
-    changeLanguage: (language) => {
-      dispatch(changeLanguage(language))
-    }
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Orders)
