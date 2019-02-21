0a568fd98ed89aba2ce55fd48d8e881ed0e8ae0e
luismendoza <luismendoza947@hotmail.com>
2019-02-21 11:26:55 -0600

    deleted drawerIcon import and added Header

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,10 @@
 import Drawer from './Drawer'
-import {
-  DrawerIcon,
-  Header
-} from './Header'
+import Header from './Header'
 import TabNav from './TabNav'
 import OrderCard from './OrderCard'
 
 export {
   Drawer,
-  DrawerIcon,
   TabNav,
   OrderCard,
   Header
