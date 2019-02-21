892d988d59852f11961bd9cb92b1d160c8212b27
luismendoza <luismendoza947@hotmail.com>
2019-02-21 17:04:28 -0600

    deleted repeated containers

--- src/containers/index.js
+++ src/containers/index.js
@@ -6,8 +6,6 @@ import ViewUser from './ViewUser'
 import EditUser from './EditUser'
 import Login from './Login'
 import Orders from './Orders'
-import InProgress from './InProgress'
-import Overdue from './Overdue'
 import Documents from './Documents'
 
 export {
@@ -19,7 +17,5 @@ export {
   EditUser,
   Login,
   Orders,
-  InProgress,
-  Overdue,
   Documents
 }
