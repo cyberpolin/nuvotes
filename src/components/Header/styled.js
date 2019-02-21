df7ef8496f648aba5e905024b4e560710fe805a9
luismendoza <luismendoza947@hotmail.com>
2019-02-20 18:04:06 -0600

    styled for header component

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,5 +1,54 @@
+import { Platform } from 'react-native'
+import { Header } from 'react-navigation'
 import styled from 'styled-components/native'
+import palette from '../../colorPalette'
+
+const isIOS = Platform.OS === 'ios'
+
+export const styles = {
+  iconMargin: {
+    marginRight: 3
+  },
+  searchContainer: {
+    width: '60%',
+    height: '100%',
+    alignItems: 'center',
+    paddingTop: 4,
+    paddingBottom: 0
+  },
+  searchInput: {
+    borderWidth: 0.5,
+    borderRadius: 20,
+    paddingHorizontal: 10
+  }
+}
 
 export const DrawerIconButton = styled.TouchableOpacity`
-  padding: 10px
+  padding: 10px;
+`
+
+export const HeaderContainer = styled.View`
+  height: ${Header.HEIGHT};
+  background-color: ${palette.white};
+  ${isIOS && 'padding-top: 18px'};
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  shadow-opacity: 1;
+  shadow-offset: 0 0;
+  shadow-color: ${palette.black};
+  shadow-radius: 1px;
+`
+
+export const HeaderTitle = styled.Text`
+  font-size: ${isIOS ? '17px' : '20px'};
+  font-weight: ${isIOS ? '600' : '500'};
+  color: ${palette.black};
+  text-align: center;
+`
+
+export const SearchButton = styled(DrawerIconButton)`
+  padding-left: 5px;
+  flex-direction: row;
+  align-items: center;
 `
