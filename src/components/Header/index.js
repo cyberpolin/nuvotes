003d08f516ba46af4bf14879129625fe223ea47e
luismendoza <luismendoza947@hotmail.com>
2019-02-21 12:56:48 -0600

    created animated input with methods and refactored some code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import {
+  Animated,
+  Dimensions
+} from 'react-native'
+import {
   Icon,
   SearchBar
 } from 'react-native-elements'
@@ -20,10 +24,11 @@ export default class Header extends Component {
     }
     this.toggleInput = this.toggleInput.bind(this)
     this.onChangeText = this.onChangeText.bind(this)
-    this.handleOnSearch = this.handleOnSearch.bind(this)
+    this.renderSearchInput = this.renderSearchInput.bind(this)
+    this.width = new Animated.Value(0)
   }
   render () {
-    const { isVisible, search } = this.state
+    const { isVisible } = this.state
     const { navigation, title } = this.props
     return (
       <HeaderContainer>
@@ -34,25 +39,8 @@ export default class Header extends Component {
           />
         </DrawerIconButton>
         {isVisible
-          ? <SearchBar
-            placeholder='Search'
-            onChangeText={this.onChangeText}
-            containerStyle={styles.searchContainer}
-            inputStyle={styles.searchInput}
-            inputContainerStyle={styles.searchInputContainer}
-            value={search}
-            lightTheme
-            onSubmitEditing={this.handleOnSearch}
-            autoCapitalize='none'
-            autoCorrect={false}
-            searchIcon={<Icon
-              name='search'
-              type='font-awesome'
-              size={20}
-              onPress={this.handleOnSearch}
-            />}
-          />
-          : <HeaderTitle>{title || 'Username'}</HeaderTitle>}
+          ? this.renderSearchInput()
+          : <HeaderTitle>{title || ''}</HeaderTitle>}
         <SearchButton onPress={this.toggleInput}>
           <Icon
             type='font-awesome'
@@ -69,15 +57,50 @@ export default class Header extends Component {
     )
   }
 
-  handleOnSearch () {
+  renderSearchInput () {
+    const { search } = this.state
+    return (
+      <Animated.View style={{width: this.width}}>
+        <SearchBar
+          placeholder='Search'
+          onChangeText={this.onChangeText}
+          containerStyle={styles.searchContainer}
+          inputStyle={styles.searchInput}
+          inputContainerStyle={styles.searchInputContainer}
+          value={search}
+          lightTheme
+          autoCapitalize='none'
+          autoCorrect={false}
+          searchIcon={<Icon
+            name='search'
+            type='font-awesome'
+            size={20}
+          />}
+        />
+      </Animated.View>
+    )
   }
 
   onChangeText (search) {
-    this.setState({ search })
+    this.setState({search})
   }
 
   toggleInput () {
     const { isVisible } = this.state
-    this.setState({ isVisible: !isVisible })
+    const inputWidth = Dimensions.get('window').width * 0.7
+    if (!isVisible) {
+      this.setState({isVisible: !isVisible})
+      const toValue = inputWidth
+      this.width.setValue(0)
+      Animated.timing(this.width, {
+        toValue
+      }).start()
+    } else {
+      const toValue = 0
+      this.width.setValue(inputWidth)
+      Animated.timing(this.width, {
+        toValue
+      }).start(() => this.setState({isVisible: !isVisible, search: ''}))
+    }
   }
 }
