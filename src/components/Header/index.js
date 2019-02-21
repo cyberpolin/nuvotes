3fdc5fb014d5931c31c4cc23bbd51f8bd1776e5e
luismendoza <luismendoza947@hotmail.com>
2019-02-20 18:04:44 -0600

    refactored DrawerIcon and created Header Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,75 @@
-import React from 'react'
-import { Icon } from 'react-native-elements'
+import React, { Component } from 'react'
 import {
-  DrawerIconButton
+  Icon,
+  SearchBar
+} from 'react-native-elements'
+import {
+  DrawerIconButton,
+  HeaderContainer,
+  HeaderTitle,
+  SearchButton,
+  styles
 } from './styled'
 
-export const DrawerIcon = (navigation) => {
-  const { toggleDrawer } = navigation.navigation
+export const DrawerIcon = ({...props}) => {
   return (
-    <DrawerIconButton onPress={() => toggleDrawer()}>
+    <DrawerIconButton {...props}>
       <Icon
         type='font-awesome'
         name='bars'
       />
     </DrawerIconButton>
   )
+}
+
+export class Header extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isVisible: false,
+      search: ''
+    }
+    this.toggleInput = this.toggleInput.bind(this)
+    this.handleSearchText = this.handleSearchText.bind(this)
+  }
+  render () {
+    const { isVisible, search } = this.state
+    const { navigation, title } = this.props
+    return (
+      <HeaderContainer>
+        <DrawerIcon onPress={navigation.toggleDrawer} />
+        {isVisible
+          ? <SearchBar
+            placeholder='Search'
+            platform='android'
+            onChangeText={this.handleSearchText}
+            containerStyle={styles.searchContainer}
+            inputStyle={styles.searchInput}
+            value={search}
+          />
+          : <HeaderTitle>Title</HeaderTitle>}
+        <SearchButton onPress={this.toggleInput}>
+          <Icon
+            type='font-awesome'
+            name='caret-left'
+            size={15}
+            containerStyle={styles.iconMargin}
+          />
+          <Icon
+            type='font-awesome'
+            name='search'
+          />
+        </SearchButton>
+      </HeaderContainer>
+    )
+  }
+
+  handleSearchText (search) {
+    this.setState({ search })
+  }
+
+  toggleInput () {
+    const { isVisible } = this.state
+    this.setState({ isVisible: !isVisible })
+  }
 }
