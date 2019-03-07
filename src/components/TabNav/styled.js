0eac337772acac96e2808858648c01fae5429f0e
luismendoza <luismendoza947@hotmail.com>
2019-03-07 12:47:52 -0600

    Changed titles, icons and styles from pending screen and search screen

--- src/components/TabNav/styled.js
+++ src/components/TabNav/styled.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components/native'
 
+export const IconContainer = {
+  paddingHorizontal: 20
+}
+
 export const Title = styled.Text`
   color: ${({color}) => color};
   align-self: center;
+  text-align: center;
+  ${({isPending}) => isPending && 'width: 200%;'};
 `
