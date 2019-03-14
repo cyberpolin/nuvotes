03ee0fd08135fcc9a9aa2ebec3fc3d9d27dd8223
luismendoza <luismendoza947@hotmail.com>
2019-03-14 12:44:49 -0600

    created helpers to generate messages for flash message

---
+++ src/helpers/messages.js
@@ -0,0 +1,22 @@
+import { translate } from '../helpers/localization'
+
+export const getMessage = type => {
+  switch (type) {
+    case 'EMPTY_FIELDS':
+      return {
+        message: translate.emptyFieldsMessage,
+        description: translate.emptyFieldsDescription,
+        type: 'warning',
+        icon: 'warning',
+        duration: 2000
+      }
+    case 'LOGIN_ERROR':
+      return {
+        message: translate.loginErrorMessage,
+        description: translate.loginErrorDescription,
+        type: 'danger',
+        icon: 'danger',
+        duration: 2000
+      }
+  }
+}
