0a25f4296610830de17df27d13aeda3ee88236ac
luismendoza <luismendoza947@hotmail.com>
2019-03-14 12:31:03 -0600

    implemented flash message

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import FlashMessage from 'react-native-flash-message'
 import Routes from './src/routes'
 import STORE from './src/store'
 import { checkLanguage } from './src/helpers/localization'
@@ -16,6 +17,7 @@ export default class App extends Component {
           onBeforeLift={checkLanguage}
         >
           <Routes />
+          <FlashMessage position='top' />
         </PersistGate>
       </Provider>
     )
