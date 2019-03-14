0365808c2f27d24f4da7824150197079026302ac
luismendoza <luismendoza947@hotmail.com>
2019-03-14 12:41:38 -0600

    combined reducers settings and user

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import language from './language'
 import search from './search'
+import user from './user'
+import settings from './settings'
 
 export default combineReducers({
   language,
-  search
+  search,
+  user,
+  settings
 })
