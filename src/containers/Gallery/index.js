d23309b6870c2deb7f955bb857bfad8396f5b43f
luismendoza <luismendoza947@hotmail.com>
2019-03-14 18:12:30 -0600

    created method to get urls of photos and pass them to the gallery render

--- src/containers/Gallery/index.js
+++ src/containers/Gallery/index.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 import ImageBrowser from 'react-native-interactive-image-gallery'
 import { Text } from 'react-native-elements'
-import {
-  Container
-} from './styled'
+import { Container } from './styled'
 import { translate } from '../../helpers/localization'
 import _ from 'lodash'
 
 export default class Gallery extends Component {
   render () {
-    const { photos } = this.props
+    const { navigation } = this.props
+    const photos = navigation.getParam('sortedPhotos', {})
+    const photosURL = this.getPhotoURL(photos)
     return (
       <Container>
         {photos && !_.isEmpty(photos)
-          ? <ImageBrowser images={photos} />
+          ? <ImageBrowser images={photosURL} />
           : <Text>{translate.noPhotos}</Text>}
       </Container>
     )
+  }
+
+  getPhotoURL (photos) {
+    return photos.map((photo, index) => {
+      return {
+        URI: photo.photo.photo,
+        id: `${index}`,
+        description: photo.status.description,
+        thumbnail: photo.photo.photo
+      }
+    })
   }
 }
