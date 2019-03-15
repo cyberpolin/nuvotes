e9f477a09ba7a31e071409c4659e44211ddd8a9e
luismendoza <luismendoza947@hotmail.com>
2019-03-14 18:13:25 -0600

    created function to fetch orders from the db, refactored function to sort and filter orders, and photo filtering

--- src/helpers/orders.js
+++ src/helpers/orders.js
@@ -1,32 +1,28 @@
 import _ from 'lodash'
-import Orders from '../../orders'
 import moment from 'moment'
+import { URL } from '../setup'
 
 const ordersByDate = orders => {
-  const sortedOrders = _.orderBy(orders, ['endDate'], ['asc'])
+  const sortedOrders = _.orderBy(orders, ['end_date'], ['asc'])
   return sortedOrders
 }
 
-export const filterOrders = orderType => {
-  if (orderType === 'Search') {
-    const filteredOrders = Orders.filter(order => {
-      return order.status === 'overdue'
-    })
-    return ordersByDate(filteredOrders)
-  } else if (orderType === 'Pending') {
-    const filteredOrders = Orders.filter(order => {
-      return order.status === 'inProgress'
+export const filterOrders = (orders, orderType) => {
+  if (orderType === 'Pending') {
+    const filteredOrders = orders.filter(order => {
+      const { description } = order.status
+      return description === 'Pending Completion'
     })
     return ordersByDate(filteredOrders)
   }
-  return ordersByDate(Orders)
+  return ordersByDate(orders)
 }
 
 export const filterOrderBySearch = (orders, search) => {
   return orders.filter(order => {
     if (!isNaN(search) && _.includes(order.id.toString(), search)) {
       return order
-    } else if (_.includes(order.name, search)) {
+    } else if (_.includes(order.number, search)) {
       return order
     }
   })
@@ -37,4 +33,42 @@ export const getDateDiff = date => {
   const momentDate = moment(date, 'YYYY-MM-DD')
   const daysDiff = momentDate.diff(today, 'days') + 1
   return daysDiff
+}
+
+export const sortPhotos = (photos, type) => {
+  return photos.filter(photo => {
+    const { status } = photo
+    return status.description === type
+  })
+}
+
+export const getOrders = (token, userId) => {
+  return dispatch => {
+    dispatch({ type: 'CHANGE_LOADING', payload: true })
+    return fetch(`${URL}get-orders/`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Token ${token}`
+      },
+      body: JSON.stringify({
+        'user_id': userId
+      })
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+      })
+      .then(jsonResponse => {
+        const { orders } = jsonResponse
+        dispatch({ type: 'POPULATE_ORDERS', payload: orders })
+        dispatch({ type: 'CHANGE_LOADING', payload: false })
+      })
+      .catch(error => {
+        console.log(error)
+        dispatch({ type: 'CHANGE_LOADING', payload: false })
+      })
+  }
 }
