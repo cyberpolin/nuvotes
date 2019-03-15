1ac5f07ee92f549fec2cf23c49ba1e4b916736e4
luismendoza <luismendoza947@hotmail.com>
2019-03-14 18:09:24 -0600

    Loading component styles file

---
+++ src/components/Loading/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  background-color: rgba(0, 0, 0, 0.4);
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+`
