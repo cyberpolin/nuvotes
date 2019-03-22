6c7704ccb3c983b11dc414943777d652db5567a0
luismendoza <luismendoza947@hotmail.com>
2019-03-22 16:23:26 -0600

    update photos action

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -1,3 +1,5 @@
+import { updateOrderPhotos } from '../helpers/orders'
+
 const initialState = []
 
 const orders = (state = initialState, { type, payload }) => {
@@ -6,6 +8,13 @@ const orders = (state = initialState, { type, payload }) => {
       return payload
     case 'EMPTY_ORDERS':
       return initialState
+    case 'UPDATE_PHOTOS':
+      const { orderId, photos } = payload
+      const orders = updateOrderPhotos(state, orderId, photos)
+      return {
+        ...state,
+        ...orders
+      }
     default:
       return state
   }
