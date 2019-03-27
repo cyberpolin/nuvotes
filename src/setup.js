cd2de38f920440767f0350fd6ca0f80cad10d4e1
luismendoza <luismendoza947@hotmail.com>
2019-03-27 09:16:51 -0600

    added new url for testing

--- src/setup.js
+++ src/setup.js
@@ -1,1 +1,3 @@
 export const URL = 'https://nuvote-wo-dev.us-west-1.elasticbeanstalk.com/api/'
+// export const URL = 'http://127.0.0.1:8000/api/'
+// export const URL = 'http://192.168.1.132:8000/api/'
