8e19c52c7a3fb2af4afd98fe7d75ce3d2ae86b2c
luismendoza <luismendoza947@hotmail.com>
2019-03-27 18:12:32 -0600

    refactored fetch function

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -1,6 +1,8 @@
 import { showMessage } from 'react-native-flash-message'
 import { getMessage } from './messages'
 import { URL } from '../setup'
+import { getOrders } from './orders'
+
 
 const getUserData = (token) => {
   return fetch(`${URL}login/`, {
@@ -40,14 +42,16 @@ export const getLogin = (username, password, navigation) => {
         if (responseJson['token']) {
           const token = responseJson.token
           getUserData(token).then(response => {
-            dispatch({ type: 'POPULATE_USER', payload: {...response, token} })
-            navigation.navigate('Home')
-            dispatch({ type: 'CHANGE_LOADING', payload: false })
-          }).catch(error => {
-            console.log('ERROR', error)
-            dispatch({ type: 'CHANGE_LOADING', payload: false })
-            const message = getMessage('ERROR')
-            showMessage(message)
+            if (response) {
+              const { id } = response
+              dispatch({ type: 'POPULATE_USER', payload: {...response, token} })
+              navigation.navigate('Home')
+              dispatch(getOrders(token, id))
+            } else {
+              dispatch({ type: 'CHANGE_LOADING', payload: false })
+              const message = getMessage('ERROR')
+              showMessage(message)
+            }
           })
         } else {
           dispatch({ type: 'CHANGE_LOADING', payload: false })
