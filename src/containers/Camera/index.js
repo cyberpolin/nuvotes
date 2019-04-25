0c1393d3c2db191b0f6ad06fdf11a2f7405c5aad
luismendoza <luismendoza947@hotmail.com>
2019-04-25 15:13:36 -0500

    added function to toggle flash and save photo

--- src/containers/Camera/index.js
+++ src/containers/Camera/index.js
@@ -3,20 +3,31 @@ import { Modal } from 'react-native'
 import { connect } from 'react-redux'
 import { RNCamera } from 'react-native-camera'
 import { Icon } from 'react-native-elements'
-import { changeCamera } from '../../actions/settings'
+import {
+  changeCamera,
+  addPhoto
+} from '../../actions/settings'
 import {
   Container,
   Options,
-  IconContainer
+  IconContainer,
+  styles
 } from './styled'
 import RF from '../../utils/responsiveFont'
+import { getFilename } from '../../helpers/orders'
 
 class Camera extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      flashOn: false
+    }
     this.changeVisibility = this.changeVisibility.bind(this)
+    this.takePhoto = this.takePhoto.bind(this)
+    this.toggleFlash = this.toggleFlash.bind(this)
   }
   render () {
+    const { flashOn } = this.state
     const { cameraOpen } = this.props.settings
     return (
       <Modal
@@ -26,18 +37,24 @@ class Camera extends Component {
       >
         <Container>
           <RNCamera
-            style={{flex: 1}}
+            style={styles.camera}
             type='back'
-            flashMode='off'
+            flashMode={flashOn ? 'on' : 'off'}
+            captureAudio={false}
+            ref={ref => {
+              this.camera = ref
+            }}
           />
           <Options>
             <IconContainer alignLeft>
               <Icon
                 type='font-awesome'
-                name='arrow-left'
+                name='times'
                 size={RF(7)}
                 onPress={this.changeVisibility}
-                color='gray'
+                color='rgba(255, 255, 255, 0.6)'
+                containerStyle={styles.backIcon}
+                underlayColor='transparent'
               />
             </IconContainer>
             <IconContainer>
@@ -45,14 +62,43 @@ class Camera extends Component {
                 type='font-awesome'
                 name='camera'
                 size={RF(7)}
-                color='gray'
+                color='rgba(255, 255, 255, 0.6)'
+                onPress={this.takePhoto}
+                underlayColor='transparent'
               />
             </IconContainer>
-            <IconContainer />
+            <IconContainer alignRight>
+              <Icon
+                type='material'
+                name={flashOn ? 'flash-on' : 'flash-off'}
+                size={RF(7)}
+                color='rgba(255, 255, 255, 0.6)'
+                underlayColor='transparent'
+                onPress={this.toggleFlash}
+              />
+            </IconContainer>
           </Options>
         </Container>
       </Modal>
     )
+  }
+
+  toggleFlash () {
+    const { flashOn } = this.state
+    this.setState({ flashOn: !flashOn })
+  }
+
+  async takePhoto () {
+    const { saveAs, addPhoto } = this.props
+    if (this.camera) {
+      var data = await this.camera.takePictureAsync()
+      const uri = data.uri
+      const filename = getFilename(uri)
+      data['filename'] = filename
+      data['mime'] = 'image/jpg'
+      data['type'] = saveAs
+      addPhoto(data)
+    }
   }
 
   changeVisibility () {
@@ -66,7 +112,8 @@ const mapStateToProps = ({ settings }) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  changeCamera: isOpen => dispatch(changeCamera(isOpen))
+  changeCamera: isOpen => dispatch(changeCamera(isOpen)),
+  addPhoto: photo => dispatch(addPhoto(photo))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Camera)
