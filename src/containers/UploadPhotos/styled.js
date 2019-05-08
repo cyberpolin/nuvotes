57181bf7721227aaf7a0d8e3bb07634f83486c79
luismendoza <luismendoza947@hotmail.com>
2019-05-08 18:09:01 -0500

    modified and created new styles

--- src/containers/UploadPhotos/styled.js
+++ src/containers/UploadPhotos/styled.js
@@ -8,7 +8,7 @@ import { heightPercentageToDP as hp } from '../../utils/layout'
 export const styles = {
   buttonStyle: {
     alignSelf: 'center',
-    height: 36,
+    height: '100%',
     backgroundColor: primary,
     borderWidth: 0,
     borderRadius: 13,
@@ -26,34 +26,43 @@ export const styles = {
   },
   badgeStyle: {
     position: 'absolute',
-    right: 4,
+    right: 2,
     top: 0
+  },
+  badgeContainer: {
+    height: 22,
+    width: 22
   },
   disabledStyle: {
     backgroundColor: 'lightgray'
   },
   disabledText: {
     color: black
+  },
+  checkboxContainer: {
+    position: 'absolute',
+    top: 0,
+    right: -10,
+    padding: 0,
+    margin: 0,
+    width: 20
+  },
+  iconContainer: {
+    position: 'absolute',
+    bottom: 20,
+    backgroundColor: primary,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    borderRadius: 30
   }
 }
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
-  justify-content: space-between;
-  padding-top: 5px;
-`
-
-export const FlexRow = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  width: 100%;
-`
-
-export const ButtonsContainer = styled.View`
   justify-content: flex-end;
-  margin-bottom: 25px;
-  margin-top: 20px;
+  padding-top: 5px;
 `
 
 export const ImagesContainer = styled.View`
@@ -64,7 +73,7 @@ export const ImagesContainer = styled.View`
   padding-horizontal: 5px;
 `
 
-export const ImageBox = styled.View`
+export const ImageBox = styled.TouchableOpacity`
   flex-basis: 49%;
 `
 
@@ -72,4 +81,38 @@ export const Photo = styled.Image`
   width: 100%;
   height: ${hp(28)};
   margin-bottom: 5px;
+`
+
+export const ButtonsBar = styled.View`
+  width: 100%;
+  flex-direction: row;
+  background-color: #5BA891;
+  height: 45px;
+`
+
+export const Flex = styled.View`
+  flex: ${({ flex }) => flex || '1'};
+`
+
+export const PhotoButton = styled.TouchableOpacity`
+  background-color: ${primary};
+  position: absolute;
+  bottom: 20;
+  width: 60px;
+  height: 60px;
+  justify-content: center;
+  border-radius: 30px;
+
+`
+
+export const IconText = styled.Text`
+`
+
+export const CenterContainer = styled.View`
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+export const DeleteButtonBar = styled.View`
 `
