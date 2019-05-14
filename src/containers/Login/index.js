f68b35dc3320449384dc9b62469b92560d25afef
luismendoza <luismendoza947@hotmail.com>
2019-05-14 10:09:03 -0500

    reset settings function

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -13,7 +13,11 @@ import { getLogin } from '../../helpers/user'
 import { getMessage } from '../../helpers/messages'
 import { getOrders } from '../../helpers/orders'
 import { getSearch } from '../../actions/search'
-import { toggleCamera, cleanPhotos } from '../../actions/settings'
+import {
+  toggleCamera,
+  cleanPhotos,
+  resetSettings
+} from '../../actions/settings'
 import {
   Container,
   LoginBox,
@@ -37,9 +41,18 @@ class Login extends Component {
   }
 
   componentWillMount () {
-    const { user, navigation, getOrders, getSearch, toggleCamera } = this.props
-    this.props.cleanPhotos()
+    const {
+      user,
+      navigation,
+      getOrders,
+      getSearch,
+      toggleCamera,
+      resetSettings,
+      cleanPhotos
+    } = this.props
+    cleanPhotos()
     toggleCamera(false)
+    resetSettings()
     if (!_.isEmpty(user)) {
       const { id, token } = user
       navigation.navigate('Home')
@@ -128,7 +141,8 @@ const mapDispatchToProps = dispatch => {
     getOrders: (token, userId) => dispatch(getOrders(token, userId)),
     getSearch: search => dispatch(getSearch(search)),
     toggleCamera: isOpen => dispatch(toggleCamera(isOpen)),
-    cleanPhotos: () => dispatch(cleanPhotos())
+    cleanPhotos: () => dispatch(cleanPhotos()),
+    resetSettings: () => dispatch(resetSettings())
   }
 }
 
