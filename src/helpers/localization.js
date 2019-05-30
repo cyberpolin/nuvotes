6076aa6dac1d80ea623c8705141a759f21883128
luismendoza <luismendoza947@hotmail.com>
2019-05-30 16:07:58 -0500

    localization commentaries

--- src/helpers/localization.js
+++ src/helpers/localization.js
@@ -9,6 +9,10 @@ export const translate = new LocalizedStrings({
   es
 })
 
+/**
+ * Checks the language from the reducer and compares
+ * it with the actual set language to change it.
+ */
 export const checkLanguage = () => {
   const { selectedLanguage } = STORE.store.getState().language
   const currentLanguage = translate.getLanguage()
@@ -17,6 +21,10 @@ export const checkLanguage = () => {
   }
 }
 
+/**
+ * Compares the actual language to change it.
+ * Needs refactoring if new translations are added.
+ */
 export const setLanguage = () => {
   const currentLanguage = translate.getLanguage()
   const languageToChange = currentLanguage === 'en' ? 'es' : 'en'
