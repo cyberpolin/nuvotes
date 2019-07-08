f1fb36e62ad0ae5bf11b88f188adc0de6f4a9d9a
luismendoza <luismendoza947@hotmail.com>
2019-07-08 10:58:11 -0500

    add photos by id

--- src/containers/Gallery/index.js
+++ src/containers/Gallery/index.js
@@ -3,6 +3,7 @@ import ImageBrowser from 'react-native-interactive-image-gallery'
 import { Text } from 'react-native-elements'
 import { Container } from './styled'
 import { translate } from '../../helpers/localization'
+import { photosByID } from '../../helpers/orders'
 import _ from 'lodash'
 
 export default class Gallery extends Component {
@@ -20,7 +21,8 @@ export default class Gallery extends Component {
   }
 
   getPhotoURL (photos) {
-    return photos.map((photo, index) => {
+    const orderedPhotos = photosByID(photos)
+    return orderedPhotos.map((photo, index) => {
       const splitted = photo.photo.photo.split('.')
       splitted.splice(splitted.length - 1, 0, 'thumbnail')
       const thumbnail = splitted.join('.')
